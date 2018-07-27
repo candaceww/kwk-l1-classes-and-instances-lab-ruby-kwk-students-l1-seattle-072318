ef52d7a7a48894961a59c8a07e8fdf59deae57c7
candaceww <candacew224@gmail.com>
2018-07-27 21:48:11 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,12 +3,13 @@ class Dog
   attr_accessor:name
   @name=name
  
-  dog_one = Dog.new 
-  dog_two = Dog.new 
-  dog_three = Dog.new 
 
  def name
     @name
   end
   
 end
+
+dog_one = Dog.new("fido")
+dog_two = Dog.new("snoopy")
+dog_three = Dog.new("lassie")
